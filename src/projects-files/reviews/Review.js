e21a31f5bb96267fcda8ad1604a3437cefc3499a
ErDalpatRathore <er.dalpat.rathore@gmail.com>
2021-05-16 09:37:36 +0530

    Reviews - added CSS styles, gradient colors and animations

--- src/projects-files/reviews/Review.js
+++ src/projects-files/reviews/Review.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
+
+const articleVariants = {
+  enter: {
+    x: "-100%",
+    opacity: 0,
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: { type: "spring", stiffness: 500, duration: 0.5 },
+      opacity: { duration: 0.5 },
+    },
+  },
+  exit: {
+    x: "100%",
+    opacity: 0,
+  },
+};
+
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const { name, job, image, text } = people[index];
+  const { id, name, job, image, text } = people[index];
   const checkNumber = number => {
     if (number > people.length - 1) {
       return 0;
@@ -35,26 +56,50 @@ const Review = () => {
 
   return (
     <article className="review">
-      <div className="img-container">
-        <img src={image} alt={name} className="person-img" />
-        <span className="quote-icon">
-          <FaQuoteRight />
-        </span>
-      </div>
-      <h4 className="author">{name}</h4>
-      <p className="job">{job}</p>
-      <p className="info">{text}</p>
-      <div className="button-container">
-        <button className="prev-btn" onClick={prevPerson}>
-          <FaChevronLeft />
-        </button>
-        <button className="next-btn" onClick={nextPerson}>
-          <FaChevronRight />
-        </button>
-      </div>
-      <button className="random-btn" onClick={randomPerson}>
-        surprise me
-      </button>
+      <AnimatePresence>
+        <div className="wrapper">
+          <motion.div
+            className="img-container"
+            key={id}
+            variants={articleVariants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+          >
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </motion.div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+
+          <div className="button-container">
+            <motion.button
+              className="prev-btn"
+              onClick={prevPerson}
+              whileHover={{ scale: 1.2 }}
+            >
+              <FaChevronLeft />
+            </motion.button>
+            <motion.button
+              className="next-btn"
+              onClick={nextPerson}
+              whileHover={{ scale: 1.2 }}
+            >
+              <FaChevronRight />
+            </motion.button>
+          </div>
+          <motion.button
+            className="random-btn"
+            onClick={randomPerson}
+            whileHover={{ scale: 1.1 }}
+          >
+            surprise me
+          </motion.button>
+        </div>
+      </AnimatePresence>
     </article>
   );
 };
--- src/projects-files/reviews/ReviewsApp.js
+++ src/projects-files/reviews/ReviewsApp.js
@@ -1,17 +1,37 @@
 import React from "react";
 import Review from "./Review";
 import "./ReviewsApp.css";
+import { motion } from "framer-motion";
+
+const sectionVariants = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+    rotate: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    rotate: 360,
+    transition: { duration: 1 },
+  },
+};
 const ReviewsApp = () => {
   return (
     <div className="reviewsApp">
       <main>
-        <section className="container">
+        <motion.section
+          className="container"
+          variants={sectionVariants}
+          initial="hidden"
+          animate="visible"
+        >
           <div className="title">
             <h2>our reviews</h2>
             <div className="underline"></div>
           </div>
           <Review />
-        </section>
+        </motion.section>
       </main>
     </div>
   );
