ea3c697f61659833c6d6a68e1d753b84bebe405a
ErDalpatRathore <er.dalpat.rathore@gmail.com>
2021-05-19 11:52:29 +0530

    Shades-Generator - contributed user friendly functionality, css styles and animations

--- src/projects-files/color-generator/ColorGeneratorApp.js
+++ src/projects-files/color-generator/ColorGeneratorApp.js
@@ -1,44 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./ColorGeneratorApp.css";
 import Values from "values.js";
 import SingleColor from "./SingleColor";
+import { motion } from "framer-motion";
+
+const formVariants = {
+  enter: {
+    x: "-100%",
+    opacity: 0,
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: { type: "spring", stiffness: 500, duration: 0.5 },
+      opacity: { duration: 0.5 },
+    },
+  },
+};
+
 const ColorGeneratorApp = () => {
+  const inputRef = useRef(null);
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values("#069").all(10));
+  const [list, setList] = useState(new Values("#006699").all(10));
+
   const handleSubmit = e => {
     e.preventDefault();
+
+    if (color.length < 3 || color.length > 8) {
+      setError(true);
+      return;
+    }
     try {
-      let colors = new Values(color).all(10);
+      let colors = new Values(`#${color}`).all(10);
       setList(colors);
+      setError(false);
     } catch (error) {
       setError(true);
       console.log("error :>> ", error);
     }
   };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <div className="colorGeneratorApp">
       <section className="container">
-        <h3>Color Generator</h3>
-        <form onSubmit={handleSubmit}>
+        <h2>Shades Generator</h2>
+        <motion.form
+          onSubmit={handleSubmit}
+          variants={formVariants}
+          initial="enter"
+          animate="center"
+        >
           <input
-            type="text"
+            ref={inputRef}
+            type="number"
             value={color}
             onChange={e => setColor(e.target.value)}
-            placeholder="#069"
+            placeholder="006699"
             className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
-            Submit
+            Generate
           </button>
-        </form>
+        </motion.form>
+        <span className="hint">
+          Hint: provide 3, 4, 6 or 8 digits input values
+        </span>
       </section>
       <section className="colors">
         {list.map((color, index) => {
-          console.log("color :>> ", color);
           return (
             <SingleColor
-              key={index}
+              key={color.hex}
               {...color}
               index={index}
               hexColor={color.hex}
--- src/projects-files/color-generator/SingleColor.js
+++ src/projects-files/color-generator/SingleColor.js
@@ -1,22 +1,54 @@
 import React, { useState, useEffect } from "react";
-import rgbTohex from "./utils";
+// import rgbTohex from "./utils";
+import { motion } from "framer-motion";
+
+const colorVariants = {
+  enter: {
+    scale: 0,
+    opacity: 0,
+  },
+  center: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+const alertVariants = {
+  enter: {
+    scale: 0,
+    opacity: 0,
+  },
+  center: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      scale: { type: "spring", stiffness: 500, duration: 0.5 },
+    },
+  },
+};
 
 const SingleColor = props => {
   const { rgb, weight, index, hexColor } = props;
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
-  const hex = rgbTohex(...rgb);
+  // const hex = rgbTohex(...rgb);
   const hexValue = `#${hexColor}`;
   useEffect(() => {
     const timeout = setTimeout(() => {
       setAlert(false);
-    }, 3000);
+    }, 1500);
     return () => {
       clearTimeout(timeout);
     };
   }, [alert]);
   return (
-    <article
+    <motion.article
+      variants={colorVariants}
+      initial="enter"
+      animate="center"
+      exit="exit"
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
       onClick={() => {
@@ -26,8 +58,17 @@ const SingleColor = props => {
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
-      {alert && <p className="alert">Copied to clipboard</p>}
-    </article>
+      {alert && (
+        <motion.span
+          className="alert"
+          variants={alertVariants}
+          initial="enter"
+          animate="center"
+        >
+          Copied to clipboard
+        </motion.span>
+      )}
+    </motion.article>
   );
 };
 
